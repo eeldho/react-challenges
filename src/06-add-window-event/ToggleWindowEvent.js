f2f2fb4b0cecc2355cb0bca4b2eee8e90eef2e1e
Emy George <emygeorge@Emys-MacBook-Air-2.local>
2024-02-07 19:39:24 -0800

    toggle-window-event

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import WindowEvent from './WindowEvent'
+import { useState, useEffect } from "react";
+import WindowEvent from "./WindowEvent";
 
-export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+export default function ToggleWindowEvent() {
+  const [windowEvent, setWindowEvent] = useState(false);
+  useEffect(() => {
+    const doubleClick = () => alert("Mouse pressed");
+    window.addEventListener("dblclick", doubleClick);
+    return () => window.removeEventListener("dblclick", doubleClick);
+  }, []);
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
+      <button onClick={() => setWindowEvent((prevState) => !prevState)}>
+        Toggle Window Event
+      </button>
       {windowEvent && <WindowEvent />}
     </div>
-  )
+  );
 }
